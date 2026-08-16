9e2df23d84eeb70905b4d6c2e06cdf5c3ac80f92
keirokeer <41898282+github-actions[bot]@users.noreply.github.com>
2026-08-16 14:15:22 +0000

    Update from keirokeer/keirokeer-pvt@ee137c1114a70a600c2c5f18d43a0db9c7e38bee

--- src/config/transcriptions-sr.js
+++
@@ -1,9 +0,0 @@
-// Serbian translations
-export const transcriptionsSr = {
-  bio: `Аудио инжењер, креатор садржаја и ентузијаста за синтезу гласа. Креатор/глас KEIRO и оснивач LUNAI Project (DiffSinger виртуелни извођачи). Доприносилац OpenUtau. Наруџбине отворене.`,
-  commissions: 'Наруџбине',
-  myPlatforms: 'Моје платформе',
-  changeLanguage: 'Промени језик'
-};
-
-export default transcriptionsSr;
